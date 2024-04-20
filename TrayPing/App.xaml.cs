b0f4f96674c0beb92884addcf1b1cf47a8f71ed3
gaoglejp <gaoglejp@gmail.com>
2024-04-20 22:06:28 +0900

    プロジェクト ファイルを追加します。

---
+++ TrayPing/App.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TrayPing
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : System.Windows.Application
+    {
+        ClassPing? pinger = null;
+        public bool _break = false;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var iconN = new System.Drawing.Icon(GetResourceStream(new Uri("/IconN.ico", UriKind.Relative)).Stream);
+            var iconR = new System.Drawing.Icon(GetResourceStream(new Uri("/IconR.ico", UriKind.Relative)).Stream);
+            var iconB = new System.Drawing.Icon(GetResourceStream(new Uri("/IconB.ico", UriKind.Relative)).Stream);
+
+            var menu = new System.Windows.Forms.ContextMenuStrip();
+            _ = menu.Items.Add("終了", null, Exit_Click);
+            var notifyIcon = new System.Windows.Forms.NotifyIcon
+            {
+                Visible = true,
+                Icon = iconN,
+                Text = "タスクトレイ常駐アプリのテストです",
+                ContextMenuStrip = menu
+            };
+            notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(NotifyIcon_Click);
+
+            Task.Run(() =>
+            {
+                while (_break == false)
+                {
+                    if (pinger == null)
+                    {
+                        System.Threading.Thread.Sleep(100);
+                        continue;
+                    }
+
+                    if (pinger == null)
+                    {
+                        notifyIcon.Icon = iconN;
+                    }
+                    else
+                    {
+                        notifyIcon.Icon = (pinger._found) ? iconB : iconR;
+                    }
+                    System.Threading.Thread.Sleep(100);
+                }
+            });
+
+        }
+
+        private void NotifyIcon_Click(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            //if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            //{
+            //    var wnd = new MainWindow();
+            //    wnd.Show();
+            //}
+
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                if (pinger != null)
+                {
+                    return;
+                }
+
+                pinger = new ClassPing();
+                pinger.start();
+
+            }
+            else if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                if (pinger != null)
+                {
+                    _break = true;
+                    pinger._break = _break;
+                }
+                System.Threading.Thread.Sleep(3000);
+                Shutdown();
+            }
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Shutdown();
+        }
+    }
+
+}
---
+++ TrayPing/ClassPing.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrayPing
+{
+    internal class ClassPing
+    {
+        public bool _break = false;
+        public bool _found = false;
+
+        public ClassPing() { }
+
+        public void start()
+        {
+            Task.Run(() =>
+            {
+                while (_break == false)
+                {
+                    //Pingオブジェクトの作成
+                    System.Net.NetworkInformation.Ping p =
+                        new System.Net.NetworkInformation.Ping();
+                    //"www.yahoo.com"にPingを送信する
+                    System.Net.NetworkInformation.PingReply reply = p.Send("192.168.0.2");
+
+                    //結果を取得
+                    if (reply.Status == System.Net.NetworkInformation.IPStatus.Success)
+                    {
+                        System.Diagnostics.Debug.WriteLine("Reply from {0}:bytes={1} time={2}ms TTL={3}",
+                            reply.Address, reply.Buffer.Length,
+                            reply.RoundtripTime, reply.Options.Ttl);
+                        _found = true;
+                    }
+                    else
+                    {
+                        System.Diagnostics.Debug.WriteLine("Ping送信に失敗。({0})",
+                            reply.Status);
+                        _found = false;
+                    }
+
+                    p.Dispose();
+
+                    System.Threading.Thread.Sleep(1000);
+                }
+            });
+        }
+    }
+}
